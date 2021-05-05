7091dd83bb0bb7617d25dbe13ac9b9dfe546b98f
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-05-05 09:27:06 -0300

    add projetos web

---
+++ JavaScript/Secção 8/JavaScript/js/meu_script.js
@@ -0,0 +1,10 @@
+
+// alert('olá, seja bem vindo ao curso!!!')
+
+/*
+	selcionar um elemento no DOM
+	atualizar o valor desse elemento com uma string
+
+*/
+document.getElementById('nome').value = 'Oi'
+
